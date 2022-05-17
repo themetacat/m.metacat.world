99e258197d2419c310d327682a7b0c872b1956c0
张宇博 <9840072+ZhangYuBoYa@user.noreply.gitee.com>
2022-05-17 22:37:15 +0800

    fix: *

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 
 
-const regex_match = /(nokia|iphone|android|motorola|^mot-|softbank|foma|docomo|kddi|up.browser|up.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte-|longcos|pantech|gionee|^sie-|portalmmm|jigs browser|hiptop|^benq|haier|^lct|operas*mobi|opera*mini|320x320|240x320|176x220)/i;
 
 
 
@@ -13,15 +12,15 @@ function MyApp({ Component, pageProps }) {
 
   React.useEffect(() => {
 
-    const u = navigator.userAgent;
-
-    const result = regex_match.exec(u);
-
-    if (result) {
-      document.location.href = "https://www.metacat.world/"
+    if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+      // 移动端
+      // document.location.href = 'https://m-metacat-world-metacat.vercel.app/';
+    } else {
+      // PC端
+      document.location.href = 'https://www.metacat.world/';
     }
 
-  }, [regex_match])
+  }, [])
   return <Component {...pageProps} />
 }
 
